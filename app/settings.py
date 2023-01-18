e429eea1552b3d642cc785ce9389489762cd4f22
Rekib Ahmed <rkb.ra0025@gmail.com>
2023-01-18 12:25:33 +0530

    DEBUG env variable

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv(bool(int("DEBUG")))
 
 ALLOWED_HOSTS = ['*']
 
